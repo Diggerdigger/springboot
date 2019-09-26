f68479522c92f4c72e8545cbfe56bace16012115
sheng <shengjunwu123@163.com>
2019-09-26 22:20:02 +0800

    q

--- src/main/java/com/august/thirteen/web/WebController.java
+++ src/main/java/com/august/thirteen/web/WebController.java
@@ -5,6 +5,7 @@ import com.august.thirteen.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -17,20 +18,27 @@ public class WebController {
         private UserMapper userMapper;
 
         @GetMapping(value = "/" )
-        public String index(HttpServletRequest request){
-            Cookie[] cookies = request.getCookies();
-            for (Cookie c:cookies) {
-               if ( c.getName().equals("token")){
-                   String token = c.getValue();
-                   System.out.println(token);
-                   UserModel user=userMapper.findUserByToken(token);
-                   request.getSession().setAttribute("user",user);
-                   break;
-               }
-
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie c:cookies) {
+            if ( c.getName().equals("token")){
+                String token = c.getValue();
+                System.out.println(token);
+                UserModel user=userMapper.findUserByToken(token);
+                request.getSession().setAttribute("user",user);
+                break;
             }
-            return "index";
+
         }
+        return "index";
+    }
+
+    @GetMapping(value = "/test" )
+    @ResponseBody
+    public String test(){
+        String s="3333333333333333";
+        return s;
+    }
 
 
 }
